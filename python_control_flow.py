3a73319528fdb84d70e67c8db98ed8df74ff3b3d
cynthia <santoro.cynthia@yahoo.com>
2023-02-12 21:12:52 -0800

    fibonacci

--- python_control_flow.py
+++ python_control_flow.py
@@ -40,7 +40,7 @@ print(f"What you entered is {len(phrase)} characters")
 # Write the code that:
 # 1. Prompts the user to enter a dog's age in human years like this:
 #      Input a dog's age in human years:
-dogsAge = input('Enter a dogs age in human year')
+dogsAge = input('Enter a dogs age in human year: ')
 dogsAge = int(dogsAge)
 # 2. Calculates the equivalent dog years, where:
 #      - The first two years count as 10 years each
@@ -95,20 +95,56 @@ else:
 #      etc.
 # Hint: The next number is found by adding the two numbers before it
 
-
-
+def fib(n):
+  a = 0
+  b = 1
+  print(a)
+  print(b)
+  
+  for i in range(2,n):
+    c = a + b
+    a = b 
+    b = c 
+    print(c)
+fib(50)
 # exercise-06 What's the  Season?
 
 # Write the code that:
 # 1. Prompts the user to enter the month (as three characters):
 #      Enter the month of the season (Jan - Dec):
+
+month= input('Enter the month of the season (Jan - Dec): ')
 # 2. Then promptshtt the user to enter the day of the month:
 #      Enter the day of the month:
+day= int(input('Enter the day of the month: '))
 # 3. Calculate what season it is based upon this chart:
 #      Dec 21 - Mar 19: Winter
 #      Mar 20 - Jun 20: Spring
 #      Jun 21 - Sep 21: Summer
 #      Sep 22 - Dec 20: Fall
+if (month == 'dec' and day in range(21,31) or 
+    month == 'jan' or 
+    month == 'fed' or 
+    month == 'mar' and day in range(1,19)):
+    print(f'{month},{day} is in Winter')
+
+elif (month == 'mar' and day in range(20,30) or 
+      month == 'apr' or 
+      month == 'may' or 
+      month == 'jun' and day in range(1,20)):
+      print(f'{month},{day} is in Spring')
+  
+elif (month == 'jun' and day in range(1,20) or 
+      month == 'jul' or 
+      month == 'aug' or 
+      month == 'sep' and day in range(1,21)):
+      print(f'{month},{day} is in Summer')
+
+elif (month == 'sep' and day in range(22,30) or
+      month =='oct' or 
+      month == 'nov' or 
+      month =='dec' and day in range(1,20)):
+      print(f'{month},{day} is in Fall')
 # 4. Print the result as follows:
 #      <Mmm> <dd> is in <season>
 
